e79d40882c7a6e6ee52c5fd3079b1b72a67274d8
Leonid Lebedev <lix.lebedev@yandex.ru>
2024-12-31 11:02:36 +0300

    add compression

--- server.cjs
+++ server.cjs
@@ -1,15 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
 
 const app = express();
+app.use(compression());
+
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Catch-all route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
